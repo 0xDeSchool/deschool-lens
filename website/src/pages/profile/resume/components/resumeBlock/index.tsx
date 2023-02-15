361efe7098d9eb8ac0fdebd90ecd251ca6a9387f
TheCleopatra <cleopatracaesar8@gmail.com>
2023-02-15 12:00:51 +0800

    feat: 完成前端简历编辑器

--- website/src/pages/profile/resume/components/resumeBlock/index.tsx
+++ website/src/pages/profile/resume/components/resumeBlock/index.tsx
@@ -1,72 +1,47 @@
-import React, { useState } from 'react'
+import React from 'react'
 import Button from 'antd/es/button'
-import type { ResumeBlockInput } from '../../types'
+import type { ResumeBlockInput, ResumeCardData } from '../../types'
 import { BlockType } from '../../enum'
 import ResumeCard from '../resumeCard'
-import CardEditor from '../cardEditor'
 
 const CAREER_TITLE = 'Career Experience'
 const EDU_TITLE = 'Education Experience'
 
 const ResumeBlock = (input: ResumeBlockInput) => {
-  const [isEditBlock, setIsEditBlock] = useState(false)
-  const [isEditCard, setIsEditCard] = useState(false)
+  const { handleEditCard, handleDeleteCard, isEditResume, dataArr, blockType, handleCreateCard } = input
   // const [editContents, setEditContents] = useState({})
-
-  const onClickButton = () => {
-    setIsEditBlock(!isEditBlock)
-  }
-
-  // TODO:
-  const handleEditCard = () => {
-    console.log('Edit Card')
-    setIsEditCard(true)
-  }
-
-  // TODO:
-  const handleDeleteCard = () => {
-    console.log('Delete Card')
-  }
-
-  // TODO:
-  const handleOk = () => {
-    console.log('Handle OK')
-    setIsEditCard(false)
-  }
-
-  // TODO:
-  const handleCancel = () => {
-    console.log('Handle Cancel')
-    setIsEditCard(false)
-  }
-
   return (
-    <div className={`w-full ${input.hasAbove ? 'mt-10' : ''}`}>
+    <div className="">
       {/* Header */}
       <div className="flex justify-between w-full items-center">
         {/* Title */}
-        <div className="text-lg font-bold">{input.blockType === BlockType.CareerBlockType ? CAREER_TITLE : EDU_TITLE}</div>
+        <div className="text-2xl font-bold">{input.blockType === BlockType.CareerBlockType ? CAREER_TITLE : EDU_TITLE}</div>
         {/* Edit / Save Button */}
-        <Button onClick={onClickButton}> {isEditBlock ? 'Save' : 'Edit'} </Button>
       </div>
 
       {/* Resume Card Entires */}
       <div>
-        {[0].map(() => (
+        {dataArr?.map((item: ResumeCardData) => (
           <ResumeCard
-            key={0}
-            isEditBlock={isEditBlock}
+            key={item.order}
+            isEditResume={isEditResume}
             handleEditCard={handleEditCard}
             handleDeleteCard={handleDeleteCard}
-            handleOk={handleOk}
-            handleCancel={handleCancel}
-            blockType={input.blockType}
+            blockType={blockType}
+            data={item}
           />
         ))}
       </div>
-
-      {/* 编辑框 */}
-      <CardEditor isEditCard={isEditCard} handleOk={handleOk} handleCancel={handleCancel} />
+      {/* 增加框 */}
+      {isEditResume && (
+        <Button
+          type="dashed"
+          className="w-full my-8"
+          onClick={() => handleCreateCard(blockType, dataArr === undefined ? 1 : dataArr.length + 1)}
+        >
+          +
+        </Button>
+      )}
     </div>
   )
 }
--- website/src/pages/profile/resume/types/index.d.ts
+++ website/src/pages/profile/resume/types/index.d.ts
@@ -1,27 +1,48 @@
+import type { Dayjs } from 'dayjs'
 import type { BlockType } from '../enum'
 
 export interface ResumeBlockInput {
   blockType: BlockType
-  hasAbove: boolean
+  dataArr: ResumeCardData[] | undefined
+  handleEditCard: (BlockType, number) => void
+  handleDeleteCard: (bt: BlockType, order: number) => void
+  isEditResume: boolean
+  handleCreateCard: (bt: BlockType, order: number) => void
 }
 
 export interface ResumeCardInput {
-  isEditBlock: boolean
-  handleEditCard: () => void
-  handleDeleteCard: () => void
-  handleOk: () => void
-  handleCancel: () => void
+  isEditResume: boolean
+  handleEditCard: (BlockType, number) => void
+  handleDeleteCard: (bt: BlockType, order: number) => void
   blockType: BlockType
+  data: ResumeCardData
 }
 
 export interface CardEditorInput {
   isEditCard: boolean
-  handleOk: () => void
+  handleOk: (newData: ResumeCardData) => void
   handleCancel: () => void
+  originalData: ResumeCardData | undefined
+  isCreateCard: boolean
 }
 
-export interface EditorDataType {
+export interface ResumeData {
+  career: ResumeCardData[] | undefined
+  edu: ResumeCardData[] | undefined
+}
+
+export interface ResumeCardData {
   title: string
-  startTime: string
-  endTime: string
+  description: string
+  startTime: Dayjs | undefined
+  endTime: Dayjs | undefined
+  proofs: SbtInfo[] | undefined
+  blockType: BlockType | undefined
+  order: number | undefined
+}
+
+export interface SbtInfo {
+  address: string
+  tokenId: number
+  img: string
 }
