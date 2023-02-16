f1ae6b33ad255599c0e67b138158a7208c3f95f0
victor <robin_fc@qq.com>
2023-02-16 20:56:33 +0800

    feat: explore 检索

--- website/src/pages/explore/index.tsx
+++ website/src/pages/explore/index.tsx
@@ -1,6 +1,8 @@
 import Input from 'antd/es/input/Input'
 import { useTranslation } from 'react-i18next'
 import AnimateBg from '~/components/animateBg'
+import type { ChangeEvent} from 'react';
+import { useState } from 'react'
 import HotCelebrities from './components/HotCelebrities'
 // import LeftoffRecord from './LeftoffRecord'
 // import HotSeries from './HotSeries'
@@ -9,6 +11,11 @@ import HotCelebrities from './components/HotCelebrities'
 
 const Explore = () => {
   const { t } = useTranslation()
+  const [searchWord, setSearchWord] = useState('')
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setSearchWord(e.target.value)
+  }
 
   return (
     <div className="w-full h-full overflow-auto relative fcc-center">
@@ -18,12 +25,12 @@ const Explore = () => {
         </div>
         <div className="w-3/4 mt-40 mb-30 h-fit relative fcs-center">
           <h1 className="text-5xl font-Anton text-white">{t('explore.title1')}</h1>
-          <Input className="mt-10 mb-5 w-2/3 min-w-300px" size="large" />
+          <Input className="mt-10 mb-5 w-2/3 min-w-300px" size="large" onChange={e => handleChange(e)} />
           <h3 className="text-2xl text-gray-5 font-ArchivoNarrow">{t('explore.searchTips')}</h3>
         </div>
       </div>
       <div className="w-full h-max fcc-center">
-        <HotCelebrities />
+        <HotCelebrities searchWord={searchWord} />
       </div>
     </div>
   )
