6a60ddfca1ed91daf4ef532fb9665b594801ab1b
TheCleopatra <cleopatracaesar8@gmail.com>
2023-02-17 20:54:51 +0800

    fix: addr normal issue

--- backend/internal/hackathon/q11e_service.go
+++ backend/internal/hackathon/q11e_service.go
@@ -3,10 +3,12 @@ package hackathon
 import (
 	"context"
 
+	"github.com/0xdeschool/deschool-lens/backend/pkg/eth"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 func (hm *HackathonManager) PutQ11e(ctx context.Context, input Q11e) primitive.ObjectID {
+	input.Address = eth.NormalizeAddress(input.Address)
 	id := hm.q11eRepo.UpsertByField(ctx, "address", input.Address, input)
 	return id
 }
