fbc975b5ff79dc76beeeffb39b62813c9b9f768b
JunaYa <ayac3j@gmail.com>
2023-02-27 19:03:37 +0800

    chore: get followers by handle

---
+++ website/src/api/cc/graphql/GetFollowingsByHandle.ts
@@ -0,0 +1,16 @@
+import { gql } from '@apollo/client'
+export const GET_FOLLOWING_BY_HANDLE = gql`
+  query getFollowersByHandle($handle: String!, $me: AddressEVM!) {
+    profileByHandle(handle: $handle) {
+      followerCount
+      isFollowedByMe(me: $me)
+      followers {
+        totalCount
+        pageInfo {
+          hasPreviousPage
+          startCursor
+          hasNextPage
+        }
+      }
+    }
+  }`;
