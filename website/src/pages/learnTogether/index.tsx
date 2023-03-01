5aef2c1f094462eb418ef60d2a8254ce6a9ab162
JunaYa <ayac3j@gmail.com>
2023-03-01 15:53:36 +0800

    chore: update header ui

--- website/src/pages/learnTogether/index.tsx
+++ website/src/pages/learnTogether/index.tsx
@@ -7,8 +7,8 @@ const LearnTogether = () => {
   console.log('recommandEvents', recommandEvents)
   return (
     <div className="relative w-auto mx-10 py-10 3xl:w-full 3xl:mx-auto 3xl:max-w-1440px 4xl:max-w-1680px h-full overflow-auto scroll-hidden">
-      <h1>Upcoming events<span>based on your profile</span></h1>
-      <p>for a more accurate match, filled more info on your booth profile</p>
+      <h1 className="text-xl font-Anton ml-8 mb-4">Upcoming events<span>based on your profile</span></h1>
+      <p className="text-gl font-ArchivoNarrow ml-8">for a more accurate match, filled more info on your booth profile</p>
       {/* recommand event list */}
       {recommandEvents?.map((item: RecomendedEvents) => {
         return (
