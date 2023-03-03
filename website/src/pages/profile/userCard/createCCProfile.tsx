24d85be8aa358bfe43108e4991bb5495c0d958db
JunaYa <ayac3j@gmail.com>
2023-03-03 15:21:25 +0800

    perf: handle mint

--- website/src/pages/profile/userCard/createCCProfile.tsx
+++ website/src/pages/profile/userCard/createCCProfile.tsx
@@ -3,13 +3,17 @@ import Input from 'antd/es/input';
 import { useState } from 'react';
 import { useAccount } from '~/context/account';
 import message from 'antd/es/message';
-import { getCCProfileNFTContract } from '~/api/cc/contract';
+import { ccContractHub } from '~/api/cc/contract';
+import { PRIMARY_PROFILE } from '~/api/cc/graphql';
+import { useLazyQuery } from '@apollo/client';
 
 const CreateCyberConnectProfile: React.FC = () => {
-  const { deschoolProfile } = useAccount();
+  const { deschoolProfile, setCyberProfile } = useAccount();
   const [handle, setHandle] = useState<string>('');
   const [loading, setLoading] = useState<boolean>(false);
+  const [getPrimaryProfile] = useLazyQuery(PRIMARY_PROFILE);
 
+  // 检查handle是否合法
   const checkHandle = () => {
     if (!handle) {
       message.warning('Please input handle')
@@ -25,7 +29,9 @@ const CreateCyberConnectProfile: React.FC = () => {
     }
     return true
   }
-  const mint = async () => {
+
+  // 创建 CyberConnect Profile
+  const handleMint = async () => {
     if (!checkHandle()) {
       return
     }
@@ -37,21 +43,16 @@ const CreateCyberConnectProfile: React.FC = () => {
         bio: deschoolProfile?.bio || '',
         handle,
         version: "1.0.0",
-      };
-      const tx = await getCCProfileNFTContract()
-        .createProfile(
-          {
-            to: deschoolProfile?.address,
-            handle,
-            metadata: JSON.stringify(metadata),
-            avatar: deschoolProfile?.avatar || '',
-            operator: "0x85AAc6211aC91E92594C01F8c9557026797493AE",
-          },
-          // preData
-          0x0,
-          // postData
-          0x0
-      );
+      }
+      const payload = {
+        to: deschoolProfile?.address,
+        handle,
+        metadata: JSON.stringify(metadata),
+        avatar: deschoolProfile?.avatar || '',
+        operator: "0x85AAc6211aC91E92594C01F8c9557026797493AE",
+      }
+      await ccContractHub().createProfile(payload, 0x0, 0x0);
+      await pollingGetCyberConnectProfile()
     } catch (error: Error | unknown) {
       console.log('error', error)
       if (error instanceof Error) {
@@ -70,13 +71,54 @@ const CreateCyberConnectProfile: React.FC = () => {
     }
   }
 
+  // 轮询获取 CyberConnect Profile, 如果获取到数据那么停止轮询
+  const pollingGetCyberConnectProfile = async () => {
+    const timer = setTimeout(async () => {
+      const userInfo = await getCyberConnectProfile()
+      if (userInfo) {
+        // if user info is available, stop polling
+        clearTimeout(timer)
+      } else {
+        // if user info is not available, try again in 1.5 seconds
+        pollingGetCyberConnectProfile()
+      }
+    }, 1500)
+  }
+
+  // 获取 CyberConnect Profile
+  const getCyberConnectProfile = async () => {
+    try {
+      const res = await getPrimaryProfile({
+        variables: {
+          address: deschoolProfile?.address,
+        },
+      })
+      const userInfo = res?.data?.address?.wallet?.primaryProfile
+      if (!userInfo) {
+        console.log('no handle')
+        return
+      }
+      setCyberProfile(userInfo)
+      return userInfo
+    } catch(e) {
+      console.log('error', e)
+    } finally {
+      setLoading(false)
+    }
+  }
+
   return (
     <div className="frc-center mt-4 mx-10">
       <Input
         style={{ border: '1px solid #6525ff' }}
         allowClear
-        placeholder='@handle' bordered={false} maxLength={20} minLength={4} onChange={(e) => setHandle(e.target.value)}/>
-      <Button type="primary" loading={loading} onClick={() => mint()}>MINT ON BSC</Button>
+        disabled={loading}
+        placeholder='@handle'
+        bordered={false}
+        maxLength={20}
+        minLength={4}
+        onChange={(e) => setHandle(e.target.value)}/>
+      <Button type="primary" loading={loading} onClick={() => handleMint()}>MINT ON BSC</Button>
     </div>
   );
 }
