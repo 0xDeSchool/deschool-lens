d0c550634cfd7fbce70be23c20b44f902d50136a
JunaYa <ayac3j@gmail.com>
2023-03-03 14:30:36 +0800

    chore: check input handle

--- website/src/pages/profile/userCard/createCCProfile.tsx
+++ website/src/pages/profile/userCard/createCCProfile.tsx
@@ -8,9 +8,30 @@ import { getCCProfileNFTContract } from '~/api/cc/contract';
 const CreateCyberConnectProfile: React.FC = () => {
   const { deschoolProfile } = useAccount();
   const [handle, setHandle] = useState<string>('');
+  const [loading, setLoading] = useState<boolean>(false);
 
+  const checkHandle = () => {
+    if (!handle) {
+      message.warning('Please input handle')
+      return false
+    }
+    if (handle.length < 4) {
+      message.warning('Handle must be at least 4 characters')
+      return false
+    }
+    if (handle.length > 32) {
+      message.warning('Handle must be at most 32 characters')
+      return false
+    }
+    return true
+  }
   const mint = async () => {
+    if (!checkHandle()) {
+      return
+    }
+
     try {
+      setLoading(true)
       const metadata = {
         name: deschoolProfile?.username || '',
         bio: deschoolProfile?.bio || '',
@@ -31,7 +52,6 @@ const CreateCyberConnectProfile: React.FC = () => {
           // postData
           0x0
       );
-
     } catch (error: Error | unknown) {
       console.log('error', error)
       if (error instanceof Error) {
@@ -45,15 +65,18 @@ const CreateCyberConnectProfile: React.FC = () => {
       } else {
         message.error('Something went wrong')
       }
+    } finally {
+      setLoading(false)
     }
   }
 
   return (
-    <div className="frc-center mt-4 mx-12">
+    <div className="frc-center mt-4 mx-10">
       <Input
         style={{ border: '1px solid #6525ff' }}
+        allowClear
         placeholder='@handle' bordered={false} maxLength={20} minLength={4} onChange={(e) => setHandle(e.target.value)}/>
-      <Button type="primary" onClick={() => mint()}>MINT ON BSC</Button>
+      <Button type="primary" loading={loading} onClick={() => mint()}>MINT ON BSC</Button>
     </div>
   );
 }
