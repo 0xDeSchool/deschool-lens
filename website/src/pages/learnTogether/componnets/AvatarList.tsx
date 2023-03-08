01b7e29ca6cc9db61ac0b46475f73ac2301e8e9a
JunaYa <ayac3j@gmail.com>
2023-03-08 14:27:34 +0800

    fix: avatar add tooltip

--- website/src/pages/learnTogether/componnets/AvatarList.tsx
+++ website/src/pages/learnTogether/componnets/AvatarList.tsx
@@ -1,4 +1,5 @@
 import Avatar from 'antd/es/avatar'
+import Tooltip from 'antd/es/tooltip'
 import { useNavigate } from 'react-router'
 import { DEFAULT_AVATAR } from '~/account'
 import { EventUserItem } from '~/api/booth/event'
@@ -19,18 +20,19 @@ const AvatarList: React.FC<AvatarListProps> = (props) => {
   return (
     <div className="frc-start flex-0 translate-x-10px">
       {avatarList?.map((v, index: number) => (
-        <Avatar
-          key={v.id}
-          src={v.avatar ? v.avatar : DEFAULT_AVATAR}
-          size={size}
-          style={{
-            transform: `translateX(-${10 * (index + 1)}px)`,
-            border: '2px solid white',
-            cursor: 'pointer',
-          }}
-          onClick={() => goProfile(v)}
-          alt={v.displayName}
-        />
+        <Tooltip key={v.id} title={v.displayName || v.name || v.address}>
+          <Avatar
+            src={v.avatar ? v.avatar : DEFAULT_AVATAR}
+            size={size}
+            style={{
+              transform: `translateX(-${10 * (index + 1)}px)`,
+              border: '2px solid white',
+              cursor: 'pointer',
+            }}
+            onClick={() => goProfile(v)}
+            alt={v.displayName}
+          />
+        </Tooltip>
       ))}
     </div>
   )
