43da1d37f6146bdd24e5804fc6a92872c940fd47
waite1x <waite1x@outlook.com>
2023-03-10 09:13:12 +0800

    fix: Update ifps geteway

--- website/src/utils/ipfs.ts
+++ website/src/utils/ipfs.ts
@@ -4,7 +4,7 @@ export function ipfsUrl(uri?: string | null): string {
     return ''
   }
   if (uri.startsWith('ipfs://')) {
-    return `https://ipfs.io/ipfs/${uri.slice(7)}`
+    return `https://deschool.mypinata.cloud/ipfs/${uri.slice(7)}`
   }
   return uri
 }
