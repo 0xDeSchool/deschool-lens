a1d32d11e029d3b898280bcf397f03b15a70f70c
JunaYa <ayac3j@gmail.com>
2023-03-28 14:45:07 +0800

    chore: generate business card

--- website/src/pages/profile/resume/components/businessCard/genCard.tsx
+++ website/src/pages/profile/resume/components/businessCard/genCard.tsx
@@ -4,7 +4,7 @@ import { message, QRCode } from 'antd';
 import { useMemo, useState } from 'react'
 import html2canvas from 'html2canvas';
 import { useTranslation } from 'react-i18next';
-import { DiscordIcon, EmailIcon } from '~/components/icon';
+import { DiscordIcon, EmailIcon, TelegramIcon } from '~/components/icon';
 import { TwitterOutlined, WechatOutlined } from '@ant-design/icons';
 import IconDeschool from '~/assets/icons/deschool.svg'
 import ShowMoreLoading from '~/components/loading/showMore';
@@ -20,7 +20,8 @@ const BusinessCard = () => {
   const [resumeCardUrl, setResumeCardUrl] = useState('')
   const { project, role, user } = useProfileResume()
 
-  const contacts = useMemo(() => user?.contacts?.filter((item) => item.name) || [], [user])
+  // email 不展示
+  const contacts = useMemo(() => user?.contacts?.filter((item) => item.name && item.contactType !== 'Email') || [], [user])
 
   // const cacheImage = (src: string) => new Promise((resolve, reject) => {
   //   const img = new Image();
@@ -48,9 +49,9 @@ const BusinessCard = () => {
       html2canvas(document.querySelector('.business-card')!, {
         useCORS: true,
         backgroundColor: '#6525FF',
-        width: 326,
+        width: 654,
         x: 0.5,
-        y: -2,
+        y: -1.5,
       }).then((canvas) => {
         let tempBlob: any
         canvas.toBlob((blob) => {
@@ -81,53 +82,54 @@ const BusinessCard = () => {
         <Button size='large' type='primary' className='absolute bottom--48px left-0 right-0 h-40px' style={{ color: '#ffffff', width: '100%', height: '40px' }} onClick={handleOk}>Generate</Button>
       </div>
       <div className='absolute z--1'>
-        <div className='business-card w-327px min-w-327px h-fit bg-gradient-to-b fcs-center from-#6525FF to-#9163FEdd text-white'>
+        <div className='business-card w-654px min-w-654px h-fit bg-gradient-to-b fcs-center from-#6525FF to-#9163FEdd text-white'>
           <img
             crossOrigin={user?.avatar?.includes('deschool.s3.amazonaws.com') ? undefined : "anonymous"}
             src={user?.avatar?.replace('https://deschool.s3.amazonaws.com/', `${location.origin}/awsResource/`)}
             alt={user?.displayName}
-            className="w-300px h-300px rounded-lg m-auto mt-14px" />
-          <div className='px-16px mt-32px mb-24px w-full frc-between'>
+            className="w-600px h-600px aspect-[1/1] rounded-lg mx-auto mt-28px" />
+          <div className='px-32px mt-64px mb-48px w-full frc-between'>
             {/* eslint-disable-next-line no-nested-ternary */}
-            <span className='font-Anton text-24px break-all'>{user?.displayName === user?.address ? getShortAddress(user?.address) : (user?.displayName && user?.displayName.length > 15 ? getShortAddress(user?.displayName) : user?.displayName)}</span>
-            <div className='inline-flex frc-start text-16px'>
+            <span className='font-Anton text-48px break-all'>{user?.displayName === user?.address ? getShortAddress(user?.address) : (user?.displayName && user?.displayName.length > 15 ? getShortAddress(user?.displayName) : user?.displayName)}</span>
+            <div className='inline-flex frc-start text-32px'>
               <img
                 crossOrigin={project?.icon?.includes('deschool.s3.amazonaws.com') ? undefined : "anonymous"}
                 src={project?.icon?.replace('https://deschool.s3.amazonaws.com/', `${location.origin}/awsResource/`)}
                 alt="project icon"
-                className='w-24px h-24px min-w-24px min-h-24px rounded-full mr-2 mt-10px' />
+                className='w-48px h-48px min-w-48px min-h-48px rounded-full mr-8px mt-18px' />
               <span className='font-ArchivoNarrow '>{project?.name}</span>
             </div>
           </div>
-          <div className='flex-1 frc-between w-full px-16px mb-32px'>
-            <div className='text-16px font-ArchivoNarrow-Medium'>{role}</div>
+          <div className='flex-1 frc-between w-full px-32px mb-64px'>
+            <div className='text-32px font-ArchivoNarrow-Medium'>{role}</div>
           </div>
-          <div className='w-full frc-center px-16px gap-16px'>
+          <div className='w-full frc-center px-32px gap-32px'>
             <div className='w-full align-middle'>
               {contacts?.map((item) => (
-                <div key={item.contactType} className="frc-start align-middle h-36px px-8px rounded-4px border-1 border-#FFFFFF20 mt-8px first:mt-0">
-                  {item.contactType === 'Discord' && <DiscordIcon style={{ fontSize: 18, color: 'white', height: 18, width: 18 }} />}
-                  {item.contactType === 'Twitter' && <TwitterOutlined style={{ fontSize: 18, color: 'white', height: 18, width: 18 }} />}
-                  {item.contactType === 'Wechat' && <WechatOutlined style={{ fontSize: 18, color: 'white', height: 18, width: 18 }} />}
-                  {item.contactType === 'Email' && <EmailIcon style={{ fontSize: 18, color: 'white', height: 18, width: 18 }} />}
-                  <span className='text-9px ml-8px mt--6px break-words align-middle'>{`${item.name}`}</span>
+                <div key={item.contactType} className="frc-start align-middle h-72px px-16px rounded-4px border-1 border-#FFFFFF20 mt-16px first:mt-0">
+                  {item.contactType === 'Discord' && <DiscordIcon style={{ fontSize: 36, color: 'white', height: 36, width: 36 }} />}
+                  {item.contactType === 'Twitter' && <TwitterOutlined style={{ fontSize: 36, color: 'white', height: 36, width: 36 }} />}
+                  {item.contactType === 'Telegram' && <TelegramIcon style={{ fontSize: 36, color: 'white', height: 36, width: 36 }} />}
+                  {item.contactType === 'Wechat' && <WechatOutlined style={{ fontSize: 36, color: 'white', height: 36, width: 36 }} />}
+                  {item.contactType === 'Email' && <EmailIcon style={{ fontSize: 36, color: 'white', height: 36, width: 36 }} />}
+                  <span className='text-20px ml-16px mt--12px break-words align-middle'>{`${item.name}`}</span>
                 </div>
               ))}
             </div>
-            <div className='min-w-120px w-120px min-h-120px h-120px self-center preload-resume-qrcode'>
+            <div className='min-w-240px w-240px min-h-240px h-240px self-center preload-resume-qrcode'>
               <QRCode
                 errorLevel="M"
-                size={120}
+                size={240}
                 color="#333333"
                 bordered={false}
                 value={`${location.origin}/resume/${user?.address}`}
-                style={{ border: 'none', borderRadius: '4px', padding: 0, margin: 0, minWidth: '120px', minHeight: '120px' }}
+                style={{ border: 'none', borderRadius: '8px', padding: 0, margin: 0, minWidth: '240px', minHeight: '240px' }}
               />
             </div>
           </div>
-          <div className='w-full frc-center align-middle mb-4 mt-6'>
-            <img src={IconDeschool} alt="deschool" className='inline' width={12} height={12} />
-            <span className="ml-2 text-white inline text-8px mt--6px font-ArchivoNarrow">DeSchool & Booth</span>
+          <div className='w-full frc-center align-middle mb-32px mt-68px'>
+            <img src={IconDeschool} alt="deschool" className='inline' width={24} height={24} />
+            <span className="ml-8px text-white inline text-16px mt--10px font-ArchivoNarrow">DeSchool & Booth</span>
           </div>
         </div>
       </div>
@@ -153,7 +155,7 @@ const BusinessCard = () => {
       >
         <div className='w-280px min-w-280px h-auto fcc-center p-0 m-0 rounded-lg'>
           {loading && <div><ShowMoreLoading /></div>}
-          {(!loading && resumeCardUrl) && <img src={resumeCardUrl} alt="resume card" className='w-full h-full rounded-lg mb-16px' />}
+          {(!loading && resumeCardUrl) && <img src={resumeCardUrl} alt="resume card" className='w-full h-full rounded-lg mb-16px object-contain' />}
           {(!loading && resumeCardUrl) && <Button type='primary' className='absolute bottom--10' style={{height: '40px', width: '280px'}} onClick={handleSave}>save</Button>}
         </div>
       </Modal>
